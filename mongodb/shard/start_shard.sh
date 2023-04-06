20826d72ac3dae4e31c686bc5b4616b8bcdfb8eb
hellio0114 <hellio0114@gmail.com>
2023-04-06 10:45:25 +0900

    shard

--- mongodb/shard/start_shard.sh
+++ mongodb/shard/start_shard.sh
@@ -8,5 +8,5 @@ mongod --config /shard/mongodShard2.conf &
 sleep 2s
 
 ps -ef | grep mongo
-sleep 3s
+sleep 4s
 netstat -ntlp
--- mongodb/shard/stop_shard.sh
+++ mongodb/shard/stop_shard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-config=$(ps -ef | grep 'mongoConfig')
-router=$(ps -ef | grep 'mongoRouter')
-shard1=$(ps -ef | grep 'mongoShard1')
-shard2=$(ps -ef | grep 'mongoShard2')
+config=$(ps -ef | grep 'mongodConfig')
+router=$(ps -ef | grep 'mongodRouter')
+shard1=$(ps -ef | grep 'mongodShard1')
+shard2=$(ps -ef | grep 'mongodShard2')
 
 
 second1=$(echo ${config} | cut -d " " -f2)
@@ -13,11 +13,11 @@ second4=$(echo ${shard2} | cut -d " " -f2)
 
 for var in $second1 $second2 $second3 $second4
 do
-	echo $var
+        echo $var
         if [ -n ${var} ]; then
-              result=$(kill -9 ${var})
-              echo process is killed.
+                result=$(kill -9 ${var})
+                echo process is killed.
         else
-              echo running process not found.
+                echo running process not found.
         fi
 done
