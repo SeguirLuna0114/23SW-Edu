7c3d56ccbd24b29337c50e37a3af09f2c572e3dd
hellio0114 <hellio0114@gmail.com>
2023-04-13 17:05:15 +0900

    selectQuery

--- node/dynamicdb/routes/main.js
+++ node/dynamicdb/routes/main.js
@@ -25,7 +25,38 @@ app.get('/hello', (req, res) => {
 app.get('/select', (req, res) => {
     const result = connection.query('select * from user');
     console.log(result);
-    res.send(result);
+    //res.send(result);
+    res.writeHead(200);
+    var template = `
+        <!doctype html>
+        <html>
+        <head>
+            <title>Result</title>
+            <meta charset="utf-8">
+            <link type="text/css" rel="stylesheet" href="mystyle.css" />
+        </head>
+        <body>
+            <table border="1" style="margin:auto; text-align:center;">
+            <thead>
+                <tr><th>User ID</th><th>Password</th></tr>
+            </thead>
+        <tbody>
+        `;
+    for (var i = 0; i < result.length; i++) {
+        template += `
+        <tr>
+            <td>${result[i]['userid']}</td>
+            <td>${result[i]['passwd']}</td>
+        </tr>
+        `;
+    }
+    template += `
+        </tbody>
+        </table>
+        </body>
+        </html>
+    `;
+    res.end(template);
 })
 
 // request O, query X
@@ -38,9 +69,79 @@ app.post('/select', (req, res) => {
 // request O, query O
 app.get('/selectQuery', (req, res) => {
     const id = req.query.id;
-    const result = connection.query("select * from user where userid=?", [id]);
-    console.log(result);
-    res.send(result);
+    if (id == "") {
+        res.writeHead(200);
+        var template = `
+    <!doctype html>
+    <html>
+    <head>
+        <title>Error</title>
+        <meta charset="utf-8">
+        <link type="text/css" rel="stylesheet" href="mystyle.css" />
+    </head>
+    <body>
+        <div>
+            <h3 style="margin-left: 30px">필드 입력</h3>
+        </div>
+    </body>
+    </html>
+    `;
+        res.end(template);
+    } else {
+        let result = connection.query("select * from user where userid=?", [id]);
+        if (result.length == 0) {
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+                <title>Error</title>
+                <meta charset="utf-8">
+                <link type="text/css" rel="stylesheet" href="mystyle.css" />
+            </head>
+            <body>
+                <div>
+                    <h3 style="margin-left: 30px">사용자가 존재하지 않음</h3>
+                </div>
+            </body>
+            </html>
+            `;
+                res.end(template);
+            } else {
+                const result = connection.query("select * from user where userid=?", [id]);
+                console.log(result);
+                //res.send(result);
+                res.writeHead(200);
+                var template2 = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>Result</title>
+                        <meta charset="utf-8">
+                        <link type="text/css" rel="stylesheet" href="mystyle.css" />
+                    </head>
+                    <body>
+                    <table border="2" style="margin:auto; text-align:center;">
+                    <thead>
+                        <tr><th>User ID</th><th>Password</th></tr>
+                    </thead>
+                    <tbody>
+                    `;
+                for (var i = 0; i < result.length; i++) {
+                    template2 += `
+                    <tr>
+                        <td>${result[i]['userid']}</td>
+                        <td>${result[i]['passwd']}</td>
+                    </tr>
+                    `;
+                }
+                template2 += `
+                    </tbody>
+                    </table>
+                    </body>
+                    </html>
+                `;
+                res.end(template2);
+            }}
 })
 
 // request O, query O
