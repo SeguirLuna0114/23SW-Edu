2c0f844e870029b39c21a3ab2ff03ef1dc2b1397
hellio0114 <hellio0114@gmail.com>
2023-04-17 09:17:29 +0900

    routes/main.js

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -70,7 +70,7 @@ function template_result(result, res) {
 }
 
 app.get('/hello', (req, res) => {
-    res.send('Hello World~!!')
+    res.send('Hello World~!!');
 })
 
 // login
@@ -100,21 +100,21 @@ app.post('/register', (req, res) => {
         if (result.length > 0) {
             res.writeHead(200);
             var template = `
-        <!doctype html>
-        <html>
-        <head>
-            <title>Error</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <div>
-                <h3 style="margin-left: 30px">Registrer Failed</h3>
-                <h4 style="margin-left: 30px">이미 존재하는 아이디입니다.</h4>
-                <a href="register.html" style="margin-left: 30px">다시 시도하기</a>
-            </div>
-        </body>
-        </html>
-        `;
+                <!doctype html>
+                <html>
+                <head>
+                    <title>Error</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <div>
+                        <h3 style="margin-left: 30px">Registrer Failed</h3>
+                        <h4 style="margin-left: 30px">이미 존재하는 아이디입니다.</h4>
+                        <a href="register.html" style="margin-left: 30px">다시 시도하기</a>
+                    </div>
+                </body>
+                </html>
+                `;
             res.end(template);
         } else {
             result = connection.query("insert into user values (?, ?)", [id, pw]);
@@ -130,7 +130,7 @@ app.get('/select', (req, res) => {
     console.log(result);
     // res.send(result);
     if (result.length == 0) {
-        template_nodata(res)
+        template_nodata(res);
     } else {
         template_result(result, res);
     }
@@ -142,7 +142,7 @@ app.post('/select', (req, res) => {
     console.log(result);
     // res.send(result);
     if (result.length == 0) {
-        template_nodata(res)
+        template_nodata(res);
     } else {
         template_result(result, res);
     }
@@ -152,14 +152,14 @@ app.post('/select', (req, res) => {
 app.get('/selectQuery', (req, res) => {
     const id = req.query.id;
     if (id == "") {
-        // res.send('User-id를 입력하세요.')
-        res.write("<script>alert('User-id를 입력하세요.');</script>");
+        // res.send('User-id를 입력하세요.');
+        res.write("<script>alert('User-id를 입력하세요.')</script>");
     } else {
         const result = connection.query("select * from user where userid=?", [id]);
         console.log(result);
         // res.send(result);
         if (result.length == 0) {
-            template_nodata(res)
+            template_nodata(res);
         } else {
             template_result(result, res);
         }
@@ -170,14 +170,14 @@ app.get('/selectQuery', (req, res) => {
 app.post('/selectQuery', (req, res) => {
     const id = req.body.id;
     if (id == "") {
-        //res.send('User-id를 입력하세요.')
+        // res.send('User-id를 입력하세요.');
         res.write("<script>alert('User-id를 입력하세요.')</script>");
     } else {
         const result = connection.query("select * from user where userid=?", [id]);
         console.log(result);
         // res.send(result);
         if (result.length == 0) {
-            template_nodata(res)
+            template_nodata(res);
         } else {
             template_result(result, res);
         }
@@ -188,32 +188,32 @@ app.post('/selectQuery', (req, res) => {
 app.post('/insert', (req, res) => {
     const { id, pw } = req.body;
     if (id == "" || pw == "") {
-        // res.send('User-id와 Password를 입력하세요.')
+        // res.send('User-id와 Password를 입력하세요.');
         res.write("<script>alert('User-id와 Password를 입력하세요.')</script>");
     } else {
         let result = connection.query("select * from user where userid=?", [id]);
         if (result.length > 0) {
             res.writeHead(200);
             var template = `
-        <!doctype html>
-        <html>
-        <head>
-            <title>Error</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <div>
-                <h3 style="margin-left: 30px">Registrer Failed</h3>
-                <h4 style="margin-left: 30px">이미 존재하는 아이디입니다.</h4>
-            </div>
-        </body>
-        </html>
-        `;
+                <!doctype html>
+                <html>
+                <head>
+                    <title>Error</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <div>
+                        <h3 style="margin-left: 30px">Registrer Failed</h3>
+                        <h4 style="margin-left: 30px">이미 존재하는 아이디입니다.</h4>
+                    </div>
+                </body>
+                </html>
+                `;
             res.end(template);
         } else {
             result = connection.query("insert into user values (?, ?)", [id, pw]);
             console.log(result);
-            res.redirect('/selectQuery?id=' + req.body.id);
+            res.redirect('/selectQuery?id=' + id);
         }
     }
 })
@@ -222,14 +222,14 @@ app.post('/insert', (req, res) => {
 app.post('/update', (req, res) => {
     const { id, pw } = req.body;
     if (id == "" || pw == "") {
-        //res.send('User-id와 Password를 입력하세요.')
+        // res.send('User-id와 Password를 입력하세요.');
         res.write("<script>alert('User-id와 Password를 입력하세요.')</script>");
     } else {
         const result = connection.query("select * from user where userid=?", [id]);
         console.log(result);
         // res.send(result);
         if (result.length == 0) {
-            template_nodata(res)
+            template_nodata(res);
         } else {
             const result = connection.query("update user set passwd=? where userid=?", [pw, id]);
             console.log(result);
@@ -243,14 +243,14 @@ app.post('/update', (req, res) => {
 app.post('/delete', (req, res) => {
     const id = req.body.id;
     if (id == "") {
-       //res.send('User-id를 입력하세요.')
-       res.write("<script>alert('User-id를 입력하세요.')</script>");
+        // res.send('User-id를 입력하세요.');
+        res.write("<script>alert('User-id를 입력하세요.')</script>");
     } else {
         const result = connection.query("select * from user where userid=?", [id]);
         console.log(result);
         // res.send(result);
         if (result.length == 0) {
-            template_nodata(res)
+            template_nodata(res);
         } else {
             const result = connection.query("delete from user where userid=?", [id]);
             console.log(result);
