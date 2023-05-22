3772b3c2c335db2ed8734217686271258daebacd
hellio0114 <seguir.luna@gmail.com>
2023-05-22 19:23:44 +0900

    Create mongoImport_UlfptcaData.py
    mongoImport_jsonfile

---
+++ python/forproject/mongoImport_UlfptcaData.py
@@ -0,0 +1,33 @@
+import json
+from pymongo import MongoClient
+from pymongo import InsertOne
+
+# MongoDB 접속 정보 설정
+client = MongoClient('mongodb://192.168.1.189:27017')
+db = client['test']  # 데이터베이스 이름 지정
+collection = db['AllUlfptcaAlarms']  # 컬렉션 이름 지정
+
+# JSON 파일 경로
+json_file = 'xxAll_UlfptcaAlarmInfo.json'
+
+# JSON 파일 로드
+with open(json_file, 'r', encoding='utf-8') as f:
+    json_data = json.load(f)
+
+# Bulk Write 작업용 리스트 생성
+bulk_operations = []
+
+# 대용량 데이터를 작은 배치로 분할하여 Bulk Write 작업 생성
+for data in json_data:
+    bulk_operations.append(InsertOne(data))
+    if len(bulk_operations) >= 1000:  # 작은 배치 크기 설정
+        collection.bulk_write(bulk_operations, ordered=False)
+        bulk_operations = []
+
+# 남은 데이터에 대해 Bulk Write 작업 실행
+if bulk_operations:
+    collection.bulk_write(bulk_operations, ordered=False)
+
+# 연결 종료
+client.close()
+print('finished!!')
