964bd86c3bc2cb3b31dba991cc470a3ff0a84012
hellio0114 <127065077+hellio0114@users.noreply.github.com>
2023-05-27 12:10:46 +0900

    Create CNCapaData_Transposed_json.py
    csvFile to JSONfile

---
+++ python/forproject/CNCapaData_Transposed_json.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import json
+
+#CNCapaData_Transposed_All.csv -> CNCapaData_Transposed_All.json
+csv_data = pd.read_csv("CNCapaData_Transposed_All.csv", sep=",", skiprows=7, engine='python')
+#skiprows: 위에서부터 n개의 행 삭제
+data_dict = csv_data.to_dict(orient="records")
+final_dict = {"CNCapaData_All": data_dict}
+
+with open("CNCapaData_Transposed_All.json", "w", encoding="utf-8") as json_file:
+    json.dump(final_dict, json_file, ensure_ascii=False)
+
+#CNCapaData_Transposed.csv -> CNCapaData_Transposed.json
+csv_data2 = pd.read_csv("CNCapaData_Transposed.csv", sep=",", skiprows=7, engine='python')
+#skiprows: 위에서부터 n개의 행 삭제
+data_dict2 = csv_data2.to_dict(orient="records")
+final_dict2 = {"CNCapaData_All": data_dict2}
+
+with open("CNCapaData_Transposed.json", "w", encoding="utf-8") as json_file2:
+    json.dump(final_dict2, json_file2, ensure_ascii=False)
