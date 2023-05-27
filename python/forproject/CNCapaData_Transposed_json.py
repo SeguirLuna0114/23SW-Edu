5d27ad3bded2feea4e536816e65bbf6ef330745a
hellio0114 <127065077+hellio0114@users.noreply.github.com>
2023-05-27 12:12:36 +0900

    Update CNCapaData_Transposed_json.py

--- python/forproject/CNCapaData_Transposed_json.py
+++ python/forproject/CNCapaData_Transposed_json.py
@@ -14,7 +14,7 @@ with open("CNCapaData_Transposed_All.json", "w", encoding="utf-8") as json_file:
 csv_data2 = pd.read_csv("CNCapaData_Transposed.csv", sep=",", skiprows=1, engine='python')
 #skiprows: 위에서부터 n개의 행 삭제
 data_dict2 = csv_data2.to_dict(orient="records")
-final_dict2 = {"CNCapaData_All": data_dict2}
+final_dict2 = {"CNCapaData": data_dict2}
 
 with open("CNCapaData_Transposed.json", "w", encoding="utf-8") as json_file2:
     json.dump(final_dict2, json_file2, ensure_ascii=False)
